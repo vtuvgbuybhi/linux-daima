579926761e2cee1e6043888aea3f2d0f3c561298
vtuvgbuybhi <2245103729@qq.com>
2023-05-07 15:57:27 +0800

    程序地址空间

---
+++ 5.6/proc.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+
+//int main()
+//{
+//  for(int i=0;i<140;i++)
+//  {
+//    printf("%d:%s\n",i,strerror(i));
+//  }
+//  return 123;
+//}
+
+
+
+//using namespace std;
+//int g_unval;
+//bint g_val=100;
+//int main()
+//{
+//  const char*s="hello world";
+//  printf("code addr: %p\n",main);
+//  printf("string rdonly addr: %p\n",s);
+//  printf("uninit addr: %p\n",&g_unval);
+//  printf("init addr: %p\n",&g_val);
+//
+//  char *heap=(char*)malloc(10);
+//  char *heap1=(char*)malloc(10);
+//  char *heap2=(char*)malloc(10);
+//  char *heap3=(char*)malloc(10);
+//  char *heap4=(char*)malloc(10);
+//
+//  printf("heap addr: %p\n",heap1);
+//  printf("heap addr: %p\n",heap2);
+//  printf("heap addr: %p\n",heap3);
+//  printf("heap addr: %p\n",heap4);
+//  
+//  printf("stack addr:%p\n",&s);
+//  printf("stack addr:%p\n",&heap);
+//  int a=10;
+//  int b=30;
+//  printf("stack addr:%p\n",&a);
+//  printf("stack addr:%p\n",&b);
+
+//  return 0;
+//}
+
